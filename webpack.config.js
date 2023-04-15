98946f25b48e429afdbc94a3f0fee6d10be84281
alvaro-bm <=>
2023-04-14 19:46:13 -0600

    he hecho cambios para poder usar la app

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,43 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  // APP ENTRY POINT
-  entry: path.join(__dirname, 'src', 'app', 'index.js'),
-
-  // OUTPUT DIRECTORY
-  output: {
-    path: path.join(__dirname, 'src', 'public'),
-    filename: 'bundle.js'
-  },
-
-  // EVIROMENT MODE
-  mode: process.env.NODE_ENV || 'development',
-  devtool: "eval-source-map",
-
-  // VALID THE EXTENSIONS FOR ES5/6 AND REACT(JSX)
-  // LOADERS
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(s(a|c)ss)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
-    ],
-  },
-  plugins: [new MiniCssExtractPlugin()],
-
-  // PATH RESOLVE
-  resolve: {
-    extensions: ['.js', '.json', '.jsx'],
-    modules: [path.resolve(__dirname, 'src'), 'node_modules']
-  }
-};
+    entry: path.join(__dirname, "src/app", "index.jsx"),
+    output: {
+        path: path.join(__dirname, "src/public"),
+        filename: "main.js"
+    },
+    mode: process.env.NODE_ENV || "development",
+    module: {
+        rules: [
+            {
+                test: /\.js|\.jsx$/,
+                exclude: /node_module/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.woff|\.woff2|\.svg|\.jpg|\.png$/,
+                use: {
+                    loader: "url-loader"
+                }
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".json", ".jsx"],
+        modules: [
+            path.resolve(__dirname, "src"),
+            "node_modules"
+        ]
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+        port: 9000
+    },
+    plugins: [new MiniCssExtractPlugin()]
+}
